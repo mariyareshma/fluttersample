05141fe2341fcdfc362685f377d528c081436d93
JP <tskarthikus@gmail.com>
2023-02-14 10:05:55 +0530

    create a material widget

---
+++ lib/medicalcare.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Medicalcare extends StatelessWidget {
+const Medicalcare({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Medicalcare',
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
